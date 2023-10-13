a2b7c2bc013478667e43f02170114ead76ad87b0
yayauheny <work.evles@gmail.com>
2023-10-13 12:16:00 +0300

    add test for passwordhasher

---
+++ src/test/java/io/ylab/walletservice/api/PasswordHasherTest.java
@@ -0,0 +1,40 @@
+package io.ylab.walletservice.api;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+class PasswordHasherTest {
+
+    @Test
+    @DisplayName("should hash password and return byte[] password")
+    void shouldReturnHashedPassword() {
+        String password = "test password!!!";
+        byte[] hashedPassword = PasswordHasher.hashPassword(password);
+
+        assertThat(hashedPassword).isNotNull().hasSizeGreaterThan(0);
+    }
+
+    @Test
+    @DisplayName("should compare different passwords and return false")
+    void shouldReturnFalseWhenCompareDifferentPasswords() {
+        String password = "fakePassword";
+        String originalPassword = "original";
+
+        byte[] hashedPassword = PasswordHasher.hashPassword(originalPassword);
+
+        assertThat(PasswordHasher.checkPassword(password, hashedPassword)).isFalse();
+    }
+    @Test
+    @DisplayName("should compare the same passwords and return true")
+    void shouldReturnTrueWhenCompareSamePasswords() {
+        String password = "original";
+        String originalPassword = "original";
+
+        byte[] hashedPassword = PasswordHasher.hashPassword(originalPassword);
+
+        assertThat(PasswordHasher.checkPassword(password, hashedPassword)).isTrue();
+    }
+}
